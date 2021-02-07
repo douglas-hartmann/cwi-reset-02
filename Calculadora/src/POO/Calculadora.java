aee479e176394cbeb1d3b94c3bb46240871455ff
Douglas Hartmann <douglasr.hartmann@gmail.com>
2021-02-07 17:36:06 -0300

    Adicionado exercício 1 (calculadora)

---
+++ Calculadora/src/POO/Calculadora.java
@@ -0,0 +1,21 @@
+package POO;
+
+public class Calculadora {
+
+    public float soma(float valor1, float valor2){
+        return valor1+valor2;
+    }
+
+    public float subtracao(float valor1, float valor2){
+        return valor1-valor2;
+    }
+
+    public float divisao(float valor1, float valor2){
+        return valor1/valor2;
+    }
+
+    public float multiplicacao(float valor1, float valor2){
+        return valor1*valor2;
+    }
+
+}
