e1233f52e3c5a4deb373f499991149d68d062517
Douglas Hartmann <douglasr.hartmann@gmail.com>
2021-02-08 13:50:05 -0300

    Incluido a calsse Scanner para entrada de dados via teclado

--- NotaAluno/src/POO/Aluno.java
+++ NotaAluno/src/POO/Aluno.java
@@ -7,14 +7,17 @@ public class Aluno {
 
     //construtor
     public Aluno(String nomeAluno) {
+
         this.nomeAluno = nomeAluno;
     }
 
     public void calcularMedia(float nota1, float nota2) {
+
         this.notaFinal = (nota1 + nota2)/2;
     }
 
     public boolean conceito(){
+
         return this.notaFinal>=7;
     }
 
